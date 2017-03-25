5029c778c2c86a48a8bfd7c6c20c888d56fba208
Declan Ramsay <declandr@gmail.com>
2017-03-25 13:29:11 +1300

    Initial Commit

---
+++ index.js
@@ -0,0 +1,16 @@
+var functions = require('firebase-functions');
+require('dotenv').config();
+var Twitter = require('twitter');
+
+var client = new Twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
+// // Start writing Firebase Functions
+// // https://firebase.google.com/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// })
