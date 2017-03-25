0094b48126890887d14f2502506f0df2d248f66a
Declan <declan@alphero.com>
2017-03-25 13:52:00 +1300

    Create my first endpoint

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ var client = new Twitter({
   access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
+
+exports.twitterFeed = functions.https.onRequest((req, res) => {
+  res.send('TwitterFeed API will be sent here!');
+});
 // // Start writing Firebase Functions
 // // https://firebase.google.com/functions/write-firebase-functions
 //
