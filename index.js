bda12fa3aba2cc78b6dbecd7abcd60c5ee5c0b71
Declan <declan@alphero.com>
2017-03-25 14:31:31 +1300

    Hook up to Twitter REST API

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 var functions = require('firebase-functions');
-require('dotenv').config();
 var Twitter = require('twitter');
 
 var client = new Twitter({
-  consumer_key: process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  consumer_key: functions.config().twitter.consumer.key,
+  consumer_secret: functions.config().twitter.consumer.secret,
+  access_token_key: functions.config().twitter.token.key,
+  access_token_secret: functions.config().twitter.token.secret
 });
 
 exports.twitterFeed = functions.https.onRequest((req, res) => {
-  res.send('TwitterFeed API will be sent here!');
+  let screenName = 'thelucideffect';
+  return client.get('statuses/user_timeline', { screen_name: screenName, count: 2})
+    .then((tweets) => {
+      res.status(200);
+      res.send(tweets);
+      return;
+    })
+    .catch((err) => {
+      res.status(400);
+      res.send(err);
+      return;
+    });
 });
-// // Start writing Firebase Functions
-// // https://firebase.google.com/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// })
